50e6e040a6dce5276a82dc886faa75ce58b43b95
Bellybutn278 <75559276+Bellybutn278@users.noreply.github.com>
2021-03-28 18:57:00 -0500

    Initialize wait-for-child-nodes

---
+++ src/Shared/BinaryWriterExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Build.Shared
+{
+    internal static class BinaryWriterExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteOptionalString(this BinaryWriter writer, string value)
+        {
+            if (value == null)
+            {
+                writer.Write((byte)0);
+            }
+            else
+            {
+                writer.Write((byte)1);
+                writer.Write(value);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteTimestamp(this BinaryWriter writer, DateTime timestamp)
+        {
+            writer.Write((Int64)timestamp.Ticks);
+            writer.Write((Int32)timestamp.Kind);
+        }
+    }
+}
