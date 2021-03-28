06e699deb4ce5cb1a77a2b1bc24e428bfc34f060
Bellybutn278 <75559276+Bellybutn278@users.noreply.github.com>
2021-03-28 18:52:48 -0500

    Initialize exp/outputpath-ensuretrailingslash

---
+++ src/Build/Evaluation/IMetadatum.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Evaluation
+{
+    /// <summary>
+    /// This interface represents a metadata object.
+    /// </summary>
+    internal interface IMetadatum
+    {
+    }
+}
